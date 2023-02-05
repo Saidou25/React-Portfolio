c61387157e080789f08437f9cccc5dc080346479
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-02-05 14:32:13 -0500

    added text and link to repo on each image within portfolio

--- my-app/src/components/pages/Portfolio.js
+++ my-app/src/components/pages/Portfolio.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../../styles.css/Portfolio.css'
+
 
 import seo from '../../images/seo.jpg';
 import weather from '../../images/weather.png';
@@ -13,53 +15,87 @@ function Project() {
     <ul className="list-group">
 
       <li className="list-group-item">
-        <img
-          alt='seo'
-          className="img-fluid"
-          src={seo}
-        />
+        <div className='container'>
+          <img
+            alt='seo'
+            className="img-fluid"
+            src={seo}
+          />
+          <div className="top-left">
+          <a href="https://github.com/Saidou25/seo-refactor"><h3>SEO Refactor</h3></a>
+            <h3>SEO Refactor</h3>
+            </div>
+        </div>
+      </li>
+
+      <li className="list-group-item">
+        <div className='container'>
+          <img
+            alt='weather'
+            className="img-fluid"
+            src={weather}
+          />
+          <div className="top-left">
+         <a href="https://github.com/Saidou25/Weather-Dashboard"><h3>Weather Dashboard</h3></a>
+          </div>
+        </div>
+      </li>
+
+      <li className="list-group-item">
+        <div className='container'>
+          <img
+            alt='sofa'
+            className="img-fluid"
+            src={sofa}
+          />
+          <div className="top-left">
+          <a href="https://github.com/kassrojas/sofa-king-cool"><h3>Sofa King Cool</h3></a>
+          
+          </div>
+        </div>
+      </li>
+
+      <li className="list-group-item">
+        <div className='container'>
+          <img
+            alt='print'
+            className="img-fluid"
+            src={print}
+          />
+          <div className="top-left">
+          <a href="https://github.com/Saidou25/Print-or-Film"><h3>Print or Film</h3></a>
+          
+          </div>
+        </div>
+      </li>
+
+      <li className="list-group-item">
+        <div className='container'>
+          <img
+            alt='team'
+            className="img-fluid"
+            src={team}
+          />
+          <div className="top-left">
+          <a href="https://github.com/Saidou25/Team"><h3>Team Generator</h3>></a>
+          
+          </div>
+        </div>
+
       </li>
       <li className="list-group-item">
-        <img
-          alt='weather'
-          className="img-fluid"
-          src={weather}
-
-        />
+        <div className='container'>
+          <img
+            alt='employee'
+            className="employee"
+            src={employee}
+          />
+           <div className="top-left">
+           <a href="https://github.com/Saidou25/Employee--Tracker"><h3>Employee Tracker</h3></a>
+           
+           </div>
+        </div>
       </li>
-      <li className="list-group-item">
-        <img
-          alt='sofa'
-          className="img-fluid"
-          src={sofa}
-
-        />
-      </li>
-      <li className="list-group-item">
-        <img
-          alt='print'
-          className="img-fluid"
-          src={print}
-
-        />
-      </li>
-      <li className="list-group-item">
-        <img
-          alt='team'
-          className="img-fluid"
-          src={team}
-
-        />
-      </li>
-      <li className="list-group-item">
-        <img
-          alt='employee'
-          className="employee"
-          src={employee}
-
-        />
-      </li>
-
     </ul>
   );
 };
