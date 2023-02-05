ae33975926033cdec270282488818b2f53932a74
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-02-05 15:24:45 -0500

    in portfolio, links to github logo and title work

--- my-app/src/components/pages/Portfolio.js
+++ my-app/src/components/pages/Portfolio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../styles.css/Portfolio.css'
 
-
+import logo from '../../images/logo.png';
 import seo from '../../images/seo.jpg';
 import weather from '../../images/weather.png';
 import sofa from '../../images/sofa.png';
@@ -22,9 +22,14 @@ function Project() {
             src={seo}
           />
           <div className="top-left">
-          <a href="https://github.com/Saidou25/seo-refactor"><h3>SEO Refactor</h3></a>
-            <h3>SEO Refactor</h3>
-            </div>
+            <a href="https://saidou25.github.io/seo-refactor/">
+              <h3>SEO Refactor</h3>
+            </a>
+            <a href="https://github.com/Saidou25/seo-refactor">
+              <img src={logo} alt="minicat"></img>
+            </a>
+
+          </div>
         </div>
       </li>
 
@@ -36,7 +41,13 @@ function Project() {
             src={weather}
           />
           <div className="top-left">
-         <a href="https://github.com/Saidou25/Weather-Dashboard"><h3>Weather Dashboard</h3></a>
+            <a href='https://saidou25.github.io/Weather-Dashboard/'>
+              <h3>Weather Dashboard</h3>
+            </a>
+
+            <a href="https://github.com/Saidou25/Weather-Dashboard">
+              <img src={logo} alt="minicat"></img>
+            </a>
           </div>
         </div>
       </li>
@@ -49,8 +60,10 @@ function Project() {
             src={sofa}
           />
           <div className="top-left">
-          <a href="https://github.com/kassrojas/sofa-king-cool"><h3>Sofa King Cool</h3></a>
-          
+            <h3>Sofa King Cool</h3>
+            <a href="https://github.com/kassrojas/sofa-king-cool">
+              <img src={logo} alt="minicat"></img>
+            </a>
           </div>
         </div>
       </li>
@@ -63,8 +76,14 @@ function Project() {
             src={print}
           />
           <div className="top-left">
-          <a href="https://github.com/Saidou25/Print-or-Film"><h3>Print or Film</h3></a>
-          
+            <a href='https://saidou25.github.io/Print-or-Film/'>
+              <h3>Print or Film</h3>
+            </a>
+
+            <a href="https://github.com/Saidou25/Print-or-Film">
+              <img src={logo} alt="minicat"></img>
+            </a>
+
           </div>
         </div>
       </li>
@@ -77,8 +96,13 @@ function Project() {
             src={team}
           />
           <div className="top-left">
-          <a href="https://github.com/Saidou25/Team"><h3>Team Generator</h3>></a>
-          
+            <a href='https://saidou25.github.io/Team/'>
+              <h3>Team Generator</h3>
+            </a>
+
+            <a href="https://github.com/Saidou25/Team">
+              <img src={logo} alt="minicat"></img>
+            </a>
           </div>
         </div>
 
@@ -90,10 +114,14 @@ function Project() {
             className="employee"
             src={employee}
           />
-           <div className="top-left">
-           <a href="https://github.com/Saidou25/Employee--Tracker"><h3>Employee Tracker</h3></a>
-           
-           </div>
+          <div className="top-left">
+            <a href='https://saidou25.github.io/Employee--Tracker/'>
+              <h3>Employee Tracker</h3>
+            </a>
+            <a href="https://github.com/Saidou25/Employee--Tracker">
+              <img src={logo} alt="minicat"></img>
+            </a>
+          </div>
         </div>
       </li>
     </ul>
