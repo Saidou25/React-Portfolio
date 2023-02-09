d6e1eec01f95a70ce961496ad18ad6d829adb5e0
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-02-09 00:18:13 -0500

    linked title and gihub logo on project images

--- my-app/src/components/Project.js
+++ my-app/src/components/Project.js
@@ -1,21 +1,31 @@
 import React from 'react';
-// import '../../styles.css/Portfolio.css'
+import '../styles.css/Project.css'
 
 function Project({ projects }) {
 
   return (
 
     <div>
-      <ul className='row display'>
-        {projects.map((project) => (
-          <li className='col-6 list-group-item' key={project.id}>
-            {`${project.title} ${project.name}`}
-            <img className='image' src='images/robot01.png' alt={project.title} />
-          </li>
-          
-        ))}
-      </ul>
-    </div>
+      <div className='container'>
+        <div className='row display'>
+          {projects.map((project) => (
+            <div className='col-6' id='effect' flex m-8 key={project.id}>
+              <div className='card flex'>
+                <img className='image' src={project.image} alt='{project.title}' />
+                <div class="card-img-overlay" id="text">
+                  <a href="https://saidou25.github.io/seo-refactor/">
+                    <h3 class="card-title">{project.title}</h3>
+                  </a>
+                  <a href="https://github.com/Saidou25/seo-refactor">
+                    <img className='logo' id="text" src='images/logo.png' alt="minicat" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div >
+    </div >
   );
 }
 
--- my-app/src/components/pages/Portfolio.js
+++ my-app/src/components/pages/Portfolio.js
@@ -13,24 +13,28 @@ const projects = [
         id: 1,
         title: 'seo',
         image : "images/seoFour.png",
+        logo: "images/logo.png,"
 },
         
 {
         id: 2,
         title: 'weather',
         image : 'images/weatherFour.png',
+        logo: "images/logo.png,"
         
 },
 {
         id: 3,
         title: 'team',
         image : "images/teamfour.png",
+        logo: "images/logo.png,"
 },
         
 {
         id: 4,
         title: 'sofa',
         image : "images/sofa.png",
+        logo: "images/logo.png,"
 },
 
 ];
