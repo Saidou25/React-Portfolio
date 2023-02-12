43badb791e8d54dd3a0ef4602f61819fadd36860
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-02-11 21:37:11 -0500

    modifictions in script to switch to gh pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,15 @@ function Footer() {
       <a href="https://github.com/Saidou25">
         <img src='images/logo.png' alt="minicat"></img>
       </a>
-      <a href="https://github.com/Saidou25/seo-refactor">
+      <a href="https://github.com/Saidou25">
        {/* I do not have a twete account yet and my stack Overflow hasn' t been active 
        so i have linded all logos to my github instead. */}
         <img src='images/twitterlogos.png' alt="minibird"></img>
+      </a>
+      <a href="https://github.com/Saidou25">
+       {/* I do not have a twete account yet and my stack Overflow hasn' t been active 
+       so i have linded all logos to my github instead. */}
+        <img src='images/linkedIn.png' className='linkedIn' alt="letters l n"></img>
       </a>
     </footer>
 
